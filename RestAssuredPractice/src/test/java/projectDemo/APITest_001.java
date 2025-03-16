ae20b72a4e0e1f9cb5f1fdb6195a7f9c30ce87b9
Sumit021995 <kaku.saurav@gmail.com>
2025-03-16 20:50:10 +0530

    new commit

---
+++ RestAssuredPractice/src/test/java/projectDemo/APITest_001.java
@@ -0,0 +1,18 @@
+package projectDemo;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+public class APITest_001 {
+	public static void main(String[] args) {
+		
+		RestAssured.baseURI="https://www.shoppersstack.com/shopping";
+		given()
+		
+		
+		.when()
+		
+		
+		.then();
+	}
+}
