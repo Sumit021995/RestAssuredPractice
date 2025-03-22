ea68d4ef8dee651f4d99f9333b6270aacb831641
Sumit021995 <kaku.saurav@gmail.com>
2025-03-22 21:14:16 +0530

    new commit

---
+++ RestAssuredPractice/src/test/java/shopperStackPayloads/ShopperProfilePayload.java
@@ -0,0 +1,13 @@
+package shopperStackPayloads;
+
+public class ShopperProfilePayload {
+	
+	public static String loginPayload()
+	{
+		return "{\r\n"
+				+ "  \"email\":\"sumitofficial021995@gmail.com\",\r\n"
+				+ "  \"password\":\"Infocus4999@\",\r\n"
+				+ "  \"role\":\"SHOPPER\"\r\n"
+				+ "}";
+	}
+}
