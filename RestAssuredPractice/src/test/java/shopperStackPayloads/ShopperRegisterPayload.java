ca3578f6679cc6df2c318778953acc46f1612057
Sumit021995 <kaku.saurav@gmail.com>
2025-04-04 23:30:58 +0530

    new commit

--- RestAssuredPractice/src/test/java/shopperStackPayloads/ShopperRegisterPayload.java
+++ RestAssuredPractice/src/test/java/shopperStackPayloads/ShopperRegisterPayload.java
@@ -3,6 +3,26 @@ package shopperStackPayloads;
 public class ShopperRegisterPayload {
 	 public static String shopperRequestPayload()
 	 {
-		 return "";
+		 String email="sumitofficial021995@gmail.com";
+		 String city="Bangalore";
+		 String country="India";
+		 String state="Karnataka";
+		 String firstName="sumitofficial021995@gmail.com";
+		 String LastName="sumitofficial021995@gmail.com";
+		 String password="Infocus";
+		 String phone="9818425197";
+		 
+		 return "{\r\n"
+		 		+ "  \"city\": \"string\",\r\n"
+		 		+ "  \"country\": \"string\",\r\n"
+		 		+ "  \"email\": \"string\",\r\n"
+		 		+ "  \"firstName\": \"string\",\r\n"
+		 		+ "  \"gender\": \"MALE\",\r\n"
+		 		+ "  \"lastName\": \"string\",\r\n"
+		 		+ "  \"password\": \"string\",\r\n"
+		 		+ "  \"phone\": 0,\r\n"
+		 		+ "  \"state\": \"string\",\r\n"
+		 		+ "  \"zoneId\": \"string\"\r\n"
+		 		+ "}";
 	 }
 }
